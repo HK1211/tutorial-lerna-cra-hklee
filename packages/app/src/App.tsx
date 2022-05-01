c17844f821df9bf1f48384783e899b69ee02b832
DevOps_user_hk-at-794756280088 <ariaslsk26@nate.com>
2022-05-01 19:43:25 +0900

    first commit

---
+++ packages/app/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Button } from '@leehankue/monorepo-test-components';
+import './App.css';
+
+function App() {
+  const [state, setState] = React.useState(0);
+  return (
+    <div className="App">
+     {state}
+     <Button onClick={()=>setState(s=>s+1)}>Click me!</Button>
+    </div>
+  );
+}
+
+export default App;
